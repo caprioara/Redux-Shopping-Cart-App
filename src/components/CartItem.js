f57de8f34ae36d259c2910da4c4b4f004f985b2d
caprioara <dragos.neatax@gmail.com>
2022-04-13 10:12:26 +0300

    cart + & -

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,10 +4,8 @@ import "./Cart.css";
 import { cartActions } from "./../store/cart-slice";
 const CartItem = ({ name, quantity, total, price, id }) => {
   const dispatch = useDispatch();
-  const removeHandler = () => {
-    dispatch(cartActions.removeFromCart(id));
-  };
-  const addHandler = () => {
+
+  const incrementCartItem = () => {
     dispatch(
       cartActions.addToCart({
         id,
@@ -15,18 +13,22 @@ const CartItem = ({ name, quantity, total, price, id }) => {
         price,
       })
     );
-  };
+  }
+  const dedcrementCartItem = () => {
+    dispatch(cartActions.removeFromCart(id))
+  }
+
   return (
     <div className="cartItem">
       <h2> {name}</h2>
       <p>${price} /-</p>
       <p>x{quantity}</p>
       <article>Total ${total}</article>
-      <button className="cart-actions" onClick={removeHandler}>
-        -
-      </button>
-      <button className="cart-actions" onClick={addHandler}>
+      <button onClick={incrementCartItem} className="cart-actions">
         +
+      </button>
+      <button onClick={dedcrementCartItem} className="cart-actions">
+        -
       </button>
     </div>
   );
